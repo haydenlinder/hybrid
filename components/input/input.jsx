d382c9c8be6c90996bceae410ce269a474a8d0ea
haydenlinder <haydenlinder@gmail.com>
2020-10-01 17:31:12 -0700

    Create Input component

---
+++ components/input/input.jsx
@@ -0,0 +1,11 @@
+import {TextInput} from 'react-native';
+import React from 'react';
+import style from './style';
+
+let Input = () => {
+    return(
+        <TextInput style={style}/>
+    )
+};
+
+export default Input;
---
+++ components/input/style.js
@@ -0,0 +1,10 @@
+const style = {
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: 300,
+    height: 40,
+    borderRadius: 20,
+};
+
+export default style;
